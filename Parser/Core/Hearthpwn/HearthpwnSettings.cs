a04e65d1fa12aae7ecff3aed571d6fb7fa78359b
Maksim <maksgolovkin89@gmail.com>
2018-05-05 10:21:27 +0300

    Add project files.

---
+++ Parser/Core/Hearthpwn/HearthpwnSettings.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Parser.Core.Hearthpwn
+{
+    class HearthpwnSettings : IParserSettings
+    {
+        public string BaseUrl { get; set; } = "https://www.hearthpwn.com/";
+        public string Prefix { get; set; } = "page";
+        public int Startpoint { get ; set ; }
+        public int Endpoint { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Parser/Core/HtmlLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+
+namespace Parser.Core
+{
+    class HtmlLoader
+    {
+        readonly HttpClient client;
+        readonly string url;
+        public HtmlLoader(IParserSettings settings)
+        {
+            client = new HttpClient();
+            url = $"{settings.BaseUrl}/{settings.Prefix}/";
+
+        }
+        public async Task<string> GetSourceByPageId( int id)
+        {
+            var currentUrl = url.Replace("{CurrentId}", id.ToString());
+            var response = await client.GetAsync(currentUrl);
+
+            if (response!=null && response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+             
+
+            }
+        }
+    }
+}
